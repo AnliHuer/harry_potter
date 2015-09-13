c51e73dbd0687bf738d1a211933c5ebfa632d2c2
AnliHuer <AnliHuer@outlook.com>
2015-09-13 20:44:44 +0800

    create book-spec-js finish the test of Book constructor

---
+++ spec/book-spec.js
@@ -0,0 +1,13 @@
+var Book = require('../src/book.js');
+
+describe('Book',function(){
+  describe('test Book constructor',function(){
+    it('should have attribute no and price',function(){
+      var expect_no = 1001;
+      var expect_price = 8;
+      var book = new Book(1001);
+      expect(book.no).toEqual(expect_no);
+      expect(book.price).toEqual(expect_price);
+    });
+  });
+});
