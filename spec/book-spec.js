007d20a57b6d640acf252ec06cd7d7586af1e4ab
AnliHuer <AnliHuer@outlook.com>
2015-09-21 00:55:59 +0800

    add the test for the function of  getPrice and getNumber in Book

--- spec/book-spec.js
+++ spec/book-spec.js
@@ -1,13 +1,23 @@
 var Book = require('../src/book.js');
 
 describe('Book', function() {
-  describe('test Book constructor', function() {
+
+  describe('test the function of getNumber', function() {
+
     it('should have attribute number and price', function() {
-      var expect_number = 1001;
-      var expect_price = 8;
       var book = new Book(1001);
-      expect(book.number).toEqual(expect_number);
-      expect(book.price).toEqual(expect_price);
+      var expect_number = 1001;
+      expect(book.getNumber()).toEqual(expect_number);
     });
   });
+
+  describe('test the function of getPrice', function() {
+
+    it('should have attribute number and price', function() {
+      var book = new Book(1001);
+      var expect_price = 8;
+      expect(book.getPrice()).toEqual(expect_price);
+    });
+  });
+
 });
