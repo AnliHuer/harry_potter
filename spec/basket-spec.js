d7deaa50577561c770028ec31ad98b59ea6baea3
AnliHuer <AnliHuer@outlook.com>
2015-09-21 01:04:16 +0800

    add the test example for all function in Book

--- spec/basket-spec.js
+++ spec/basket-spec.js
@@ -7,42 +7,54 @@ describe('Basket', function() {
     it('should return the attribute of basketItem', function() {
       var basketItem = basket.getBasketItem();
       expect(basketItem).toEqual([]);
+
       basket.addBasketItem(new Book(1001));
       basketItem = basket.getBasketItem();
       expect(basketItem).toEqual([{number: 1001, count: 1}]);
+
+      basket.addBasketItem(new Book(1001));
+      basketItem = basket.getBasketItem();
+      expect(basketItem).toEqual([{number: 1001, count: 2}]);
     });
   });
 
   describe('test Basket the function addBasketItem', function() {
-    var basket = new Basket();
+
     it('should add the newBookItem in basketItem', function() {
-      var book = new Book(1001);
-      var book1 = new Book(1001);
-      basket.addBasketItem(book);
+      var basket = new Basket();
+      basket.addBasketItem(new Book(1001));
       expect(basket.basketItem[0]).toEqual({
         number: 1001,
         count: 1
       });
-      basket.addBasketItem(book1);
+
+      basket.addBasketItem(new Book(1001));
       expect(basket.basketItem[0]).toEqual({
         number: 1001,
         count: 2
       });
+
+      basket.addBasketItem(new Book(1002));
+      basketItem = basket.getBasketItem();
+      expect(basketItem).toEqual([{number: 1001, count: 2},{number: 1002, count: 1}]);
     });
   });
 
   describe('test Basket the function getBookNum', function() {
-    var basket = new Basket();
+
     it('should return the num of all buyBook', function() {
-      basket.basketItem = [{
-        number: 1001,
-        count: 3
-      }, {
-        number: 1003,
-        count: 1
-      }];
+      var basket = new Basket();
       var buyBookNum = basket.getBookNum();
-      expect(buyBookNum).toEqual(4);
+      expect(buyBookNum).toEqual(0);
+
+      basket.addBasketItem(new Book(1001));
+      basket.addBasketItem(new Book(1001));
+      buyBookNum = basket.getBookNum();
+      expect(buyBookNum).toEqual(2);
+
+      basket.addBasketItem(new Book(1002));
+      buyBookNum = basket.getBookNum();
+      expect(buyBookNum).toEqual(3);
     });
   });
 });
